4110dd6bd0f6fa18c89403634e068179a9518fd6
Gregor Destrée <gregordestree@gmail.com>
2016-08-23 14:02:17 +0200

    Refactor to include class methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,41 +6,41 @@ class ItemsController < ApplicationController
   # GET /items.json
   def index
     @items = Item.all
-    @total_opened = Item.where(event:"open").length 
-    @total_clicked = Item.where(event:"click").length
+    @total_opened = Item.opened
+    @total_clicked = Item.clicked
 
     #Add code for statistics for email type - ORDER
-    open_order= Item.where(email_type:"Order", event: "open").length
-    click_order= Item.where(email_type:"Order", event: "click").length
-    total_order= Item.where(email_type:"Order").length
+    open_order= Item.statistics("Order", "open")
+    click_order= Item.statistics("Order", "click")
+    total_order= Item.total_email_type("Order")
     @open_rate_order= open_order.to_f/total_order
     @click_rate_order= click_order.to_f/total_order
 
     #Add code for statistics for email type -SHIPMENT
-    open_ship= Item.where(email_type:"Shipment", event: "open").length
-    click_ship= Item.where(email_type:"Shipment", event: "click").length
-    total_ship= Item.where(email_type:"Shipment").length
+    open_ship= Item.statistics("Shipment", "open")
+    click_ship= Item.statistics("Shipment", "click")
+    total_ship= Item.total_email_type("Shipment")
     @open_rate_shipment= open_ship.to_f/total_ship
     @click_rate_shipment= click_ship.to_f/total_ship
 
     #Add code for statistics for email type -DISCOUNT
-    open_discount= Item.where(email_type:"GetABookDiscount", event: "open").length
-    click_discount= Item.where(email_type:"GetABookDiscount", event: "click").length
-    total_discount= Item.where(email_type:"GetABookDiscount").length
+    open_discount= Item.statistics("GetABookDiscount", "open")
+    click_discount= Item.statistics("GetABookDiscount", "click")
+    total_discount= Item.total_email_type("GetABookDiscount")
     @open_rate_discount= open_discount.to_f/total_discount
     @click_rate_discount= click_discount.to_f/total_discount
 
     #Add code for statistics for email type -CONFIRM
-    open_confirm= Item.where(email_type:"UserConfirmation", event: "open").length
-    click_confirm = Item.where(email_type:"UserConfirmation", event: "click").length
-    total_confirm= Item.where(email_type:"UserConfirmation").length
+    open_confirm= Item.statistics("UserConfirmation", "open")
+    click_confirm = Item.statistics("UserConfirmation", "click")
+    total_confirm= Item.total_email_type("UserConfirmation")
     @open_rate_confirm= open_confirm.to_f/total_confirm
     @click_rate_confirm= click_confirm.to_f/total_confirm
 
     #Add code for statistics for email type -REFER
-    open_refer= Item.where(email_type:"ReferAFriend", event: "open").length
-    click_refer= Item.where(email_type:"ReferAFriend", event: "click").length
-    total_refer= Item.where(email_type:"ReferAFriend").length
+    open_refer= Item.statistics("ReferAFriend", "open")
+    click_refer= Item.statistics("ReferAFriend", "click")
+    total_refer= Item.total_email_type("ReferAFriend")
     @open_rate_refer= open_refer.to_f/total_refer
     @click_rate_refer= click_refer.to_f/total_refer
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ActiveRecord::Base
+	def self.opened
+		Item.where(event:"open").length 
+	end
+
+	def self.clicked
+		Item.where(event:"click").length 
+	end
+
+	def self.statistics(email_type, *event)
+		Item.where(email_type: email_type, event: event.join("")).length
+	end
+
+	def self.total_email_type(email_type)
+		Item.where(email_type: email_type).length
+	end
 end
